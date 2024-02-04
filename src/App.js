860db6fd040e3cdd0782f86b18506b353e803955
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-04 19:53:39 +0530

    Initial Commit With Custom JSX

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>Hey Bro This is News Website, It provides trending news from the world</div>
+    )
+  }
+}
