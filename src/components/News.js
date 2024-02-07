9d5a16c2cac8d7ffdb49c113b01890c3a11e35bb
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-07 10:19:32 +0530

    Added Author Name, Time and Badge for Source Name

--- src/components/News.js
+++ src/components/News.js
@@ -81,7 +81,7 @@ export class News extends Component {
                 <div className="row">
                     {!this.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url} style={{ display: 'flex' }}>
-                            <NewsItem title={element.title ? element.title.slice(0, 50) : ""} description={element.description ? element.description.slice(0, 100) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            <NewsItem title={element.title ? element.title.slice(0, 50) : ""} description={element.description ? element.description.slice(0, 100) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                         </div>
                     })}
                 </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,17 +3,20 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
     render() {
 
-        let { title, description, imageUrl, newsUrl } = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className='my-3'>
                 <div className="card">
+                    <span className="position-absolute top-0 start-80 translate-middle badge rounded-pill bg-primary" style={{ zIndex: '1', left: '85%' }}>
+                        {source}
+                    </span>
                     <img src={!imageUrl ? "https://images.cnbctv18.com/wp-content/uploads/2019/09/3d1bf24078a10a5b1751a8ae8c942a3d-1019x573.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className='text-muted'>By {author ? author : "unknown"} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} target='_' className="btn btn-sm btn-dark">Read More</a>
                     </div>
-
                 </div>
 
             </div>
